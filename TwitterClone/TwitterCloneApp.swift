044df7c28a6eaa18aa1df3091e318329cdb970dc
chihsien <chwu891@gmail.com>
2021-01-28 15:40:12 +0800

    Initial Commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by chihsien  on 2021/1/28.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
