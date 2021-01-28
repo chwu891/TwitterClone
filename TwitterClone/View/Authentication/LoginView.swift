1cc940794afce8a147dc67b81eef987f2bb9a552
chihsien <chwu891@gmail.com>
2021-01-28 17:46:53 +0800

    Finish LoginView.

---
+++ TwitterClone/View/Authentication/LoginView.swift
@@ -0,0 +1,86 @@
+//
+//  LoginView.swift
+//  TwitterClone
+//
+//  Created by chihsien  on 2021/1/28.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    
+    @State var email = ""
+    @State var password = ""
+    
+    var body: some View {
+        ZStack {
+            VStack {
+                Image("Twitter_Logo_WhiteOnBlue")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 300, height: 180)
+                    .padding(.top, 30)
+                
+                VStack(spacing: 20) {
+                    CustomTextField(text: $email, placeholder: Text("Email"), imageName: "envelope")
+                        .padding()
+                        .background(Color(.init(white: 1, alpha: 0.15)))
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+              
+                    SecureTextField(text: $password, placeholder: Text("Password"), imageName: "lock")
+                        .padding()
+                        .background(Color(.init(white: 1, alpha: 0.15)))
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                .padding(.horizontal)
+                
+                HStack {
+                    Spacer()
+                    
+                    Button(action: {}, label: {
+                        Text("Forgot Password?")
+                            .font(.footnote)
+                            .foregroundColor(.white)
+                            .padding()
+                    })
+                }
+                
+                Button(action: {}, label: {
+                    Text("Sign in")
+                        .font(.headline)
+                        .foregroundColor(.blue)
+                })
+                .frame(width: 360, height: 50)
+                .background(Color(.white))
+                .clipShape(Capsule())
+                
+                Spacer()
+                
+                HStack {
+                    Text("Don't have an account?")
+                        .font(.footnote)
+                        .foregroundColor(.white)
+                    
+                    Button(action: {}, label: {
+                        Text("Sign up")
+                            .font(.footnote)
+                            .bold()
+                            .foregroundColor(.white)
+                        
+                    })
+                }
+                .padding(.bottom, 30)
+            }
+        }
+        .background(Color(#colorLiteral(red: 0.1137254902, green: 0.631372549, blue: 0.9490196078, alpha: 1)))
+        .ignoresSafeArea()
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
---
+++ TwitterClone/View/Utils/CustomTextField.swift
@@ -0,0 +1,37 @@
+//
+//  CustomTextField.swift
+//  TwitterClone
+//
+//  Created by chihsien  on 2021/1/28.
+//
+
+import SwiftUI
+
+struct CustomTextField: View {
+    
+    @Binding var text: String
+    let placeholder: Text
+    let imageName: String
+    
+    var body: some View {
+        ZStack(alignment: .leading) {
+            
+            if text.isEmpty {
+                placeholder
+                    .foregroundColor(Color(.init(white: 1, alpha: 0.6)))
+                    .padding(.leading, 40)
+            }
+            
+            HStack(spacing: 16) {
+                Image(systemName: imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 20, height: 20)
+                
+                TextField("", text: $text)
+    
+            }
+        }
+    }
+}
+
